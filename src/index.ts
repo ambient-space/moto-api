ba25985a1d75f46f4ef1233a70ec528763c4dd99
Bhavishya <bhavishya1102@gmail.com>
2024-07-05 18:12:13 +0530

    feat: Add trip routes for CRUD operations and update index.ts to use Elysia authentication module

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import Elysia from "elysia"
 import { authRoutes } from "./routes/auth"
+import { tripRoutes } from "./routes/trip"
 import { userRoutes } from "./routes/user"
 
 const app = new Elysia()
 	.use(authRoutes)
 	.use(userRoutes)
+	.use(tripRoutes)
 	.get("/", () => "Hello Elysia")
 	.listen(3000)
 
