7a82b55287fae7ae91d257ea3ef03a50a1d8d45b
Bhavishya <bhavishya1102@gmail.com>
2024-07-05 15:52:05 +0530

    refactor: Update index.ts to use Elysia authentication module and add authentication and user routes

--- src/index.ts
+++ src/index.ts
@@ -1,65 +1,12 @@
-import { lucia } from "@lib/auth/lucia"
-import type { ExtendedContext } from "@lib/types"
-import { Elysia } from "elysia"
+import Elysia from "elysia"
 import { authRoutes } from "./routes/auth"
 import { userRoutes } from "./routes/user"
 
-const app = new Elysia().derive(async (context): Promise<ExtendedContext> => {
-	// CSRF check
-	// if (context.request.method !== "GET") {
-	// 	const originHeader = context.request.headers.get("Origin")
-	// 	// NOTE: You may need to use `X-Forwarded-Host` instead
-	// 	const hostHeader = context.request.headers.get("Host")
-	// 	if (
-	// 		!originHeader ||
-	// 		!hostHeader ||
-	// 		!verifyRequestOrigin(originHeader, [hostHeader])
-	// 	) {
-	// 		return {
-	// 			user: null,
-	// 			session: null,
-	// 		}
-	// 	}
-	// }
-
-	// use headers instead of Cookie API to prevent type coercion
-	const cookieHeader = context.request.headers.get("Cookie") ?? ""
-	const sessionId = lucia.readSessionCookie(cookieHeader)
-	if (!sessionId) {
-		return {
-			user: null,
-			session: null,
-		}
-	}
-
-	const { session, user } = await lucia.validateSession(sessionId)
-	if (session?.fresh) {
-		const sessionCookie = lucia.createSessionCookie(session.id)
-		context.cookie[sessionCookie.name].set({
-			value: sessionCookie.value,
-			...sessionCookie.attributes,
-		})
-	}
-	if (!session) {
-		const sessionCookie = lucia.createBlankSessionCookie()
-		context.cookie[sessionCookie.name].set({
-			value: sessionCookie.value,
-			...sessionCookie.attributes,
-		})
-	}
-	return {
-		user,
-		session,
-	}
-})
-
-app
+const app = new Elysia()
 	.use(authRoutes)
 	.use(userRoutes)
 	.get("/", () => "Hello Elysia")
 	.listen(3000)
-
-export type App = typeof app
 
 console.log(
 	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
