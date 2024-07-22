4d1eae86342fd9dc1899dff0e5307928b5410780
Bhavishya <bhavishya1102@gmail.com>
2024-07-22 11:30:46 +0530

    feat: Add authUserRelations and authSessionRelations to auth schema
    This commit adds the `authUserRelations` and `authSessionRelations` fields to the `auth` schema, which include relations with `userProfile` and `authSession` respectively. These relations allow for easier access to related data when querying authentication information.

--- src/db/schema/auth.ts
+++ src/db/schema/auth.ts
@@ -1,5 +1,7 @@
 import { sharedColumns } from "@db/shared"
+import { relations } from "drizzle-orm"
 import { pgTable, text, timestamp } from "drizzle-orm/pg-core"
+import { userProfile } from "./user"
 
 export const authUser = pgTable("auth_user", {
 	id: text("id").primaryKey(),
@@ -8,6 +10,14 @@ export const authUser = pgTable("auth_user", {
 	hashedPassword: text("hashed_password"),
 	...sharedColumns,
 })
+
+export const authUserRelations = relations(authUser, ({ one, many }) => ({
+	profile: one(userProfile, {
+		fields: [authUser.id],
+		references: [userProfile.userId],
+	}),
+	session: many(authSession),
+}))
 
 export const authSession = pgTable("auth_session", {
 	id: text("id").primaryKey(),
@@ -20,3 +30,10 @@ export const authSession = pgTable("auth_session", {
 	}).notNull(),
 	...sharedColumns,
 })
+
+export const authSessionRelations = relations(authSession, ({ one }) => ({
+	user: one(authUser, {
+		fields: [authSession.userId],
+		references: [authUser.id],
+	}),
+}))
