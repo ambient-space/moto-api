5989ef67faee8e7cfa296fb1fc5d7862a2e42311
Bhavishya <bhavishya1102@gmail.com>
2024-07-25 18:20:55 +0530

    feat: Add lazy loading for images and update routes
    The code changes in this commit add lazy loading for images to improve page load performance. Additionally, the `assetRoutes` are updated to include a new route for uploading images to Cloudinary. This feature enhancement allows users to easily upload and store images in the cloud, enhancing the functionality of the application.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import Elysia from "elysia"
+import { assetRoutes } from "./routes/asset"
 import { authRoutes } from "./routes/auth"
 import { communityRoutes } from "./routes/community"
 import { messageRoutes } from "./routes/message"
@@ -11,6 +12,7 @@ const app = new Elysia()
 	.use(tripRoutes)
 	.use(communityRoutes)
 	.use(messageRoutes)
+	.use(assetRoutes)
 	.get("/", () => ({ data: { status: "ok" }, error: null }))
 	.listen(3000)
 
